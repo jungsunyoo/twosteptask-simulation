609c54c3786781830a69964ead1575f2490497f0
jungsunyoo <yoojungsun0@gmail.com>
2021-04-18 11:59:32 -0700

    partitioned nstate for loop for parallel processing

---
+++ meta_wrapper_5.m
@@ -0,0 +1,23 @@
+clear;clc;
+
+
+
+% Meta-wrapper for executing simulations
+nrtrials = 200;
+nrits = 1000;
+% for nstates = 2:5
+% clearvars -except nstates nrtrials nrits
+nstates=5;
+%     data = wrapper_nstates(1000,nstates, nrtrials);
+data = wrapper_nstates_decay(nrits,nstates, nrtrials);
+% end
+
+% % Meta-wrapper for visualizing & saving data
+% for nstates = 2:20
+%     clearvars -except nstates 
+%     currname = ['MBMF_stochastic_1choice_rew_', num2str(nstates) ,'states_sim'];
+%     load(currname)
+%     plot_grid(data)
+%     savefig(currname)
+% end
+
